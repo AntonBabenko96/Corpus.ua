f72fdfdf0990cd6c554f9cdd931e2392f2e62b96
Ihor <ihorfront88@gmail.com>
2023-06-12 21:00:26 +0300

    add: start files

--- src/components/example/index.js
+++
@@ -1,9 +0,0 @@
-const testTitleRef = document.querySelector('.test-title');
-
-testTitleRef.addEventListener('mouseenter', e => {
-  e.target.style.color = 'red';
-});
-
-testTitleRef.addEventListener('mouseout', e => {
-  e.target.style.color = 'blue';
-});
