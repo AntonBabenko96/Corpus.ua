0e790a069d8c2b6200cbad81cdf5c4fa2f5542fc
Your Name <wtm@hotmail.com.au>
2023-06-13 22:08:17 +0300

    about us section (mobile)

--- src/app/MainPage/AboutUs/AboutUs.jsx
+++ src/app/MainPage/AboutUs/AboutUs.jsx
@@ -1,26 +1,46 @@
 import Image from 'next/image';
 import styles from './AboutUs.module.scss';
 
-
 export default function AboutUs() {
   return (
     <section>
+      <Image
+        src="/images/aboutUs/aboutUs.png"
+        priority
+        alt="корпус меблі"
+        width={343}
+        height={344}
+        className={styles.aboutUsImg}
+      />
       <div className={styles.aboutUsBackground}>
         <div className={styles.aboutUsFrame}></div>
-        <Image src="/images/aboutUs/aboutUs.png"  priority alt="aaa" width={358} height={358} className={styles.aboutUsImg} />
-     
-        {/* <img src="../public/images/aboutUs.mainmob@1x.png" alt="corpus.about us" className={styles.aboutUsImg} /> */}
-        <h2 className={styles.aboutUstitle}>KORPUS</h2>
-        <p className={styles.aboutUsContent}>
-          Ми команда талановитих дизайнерів та майстрів, що втілюють естетику та
-          комфорт у вашому будинку. Наша місія - зробити ваше життя комфортним
-          та елегантним, створюючи індивідуальні рішення, які поєднують
-          функціональність та естетичність. Завдяки ретельному вибору матеріалів
-          найвищої якості, ми забеспечуємо довговічність та естетичність наших
-          виробів.
-        </p>
+
+        <h2 className={styles.aboutUsTitle}>KORPUS</h2>
+        <div className={styles.aboutUsText}>
+          <p className={styles.aboutUsContent}>
+            Ми команда талановитих дизайнерів та майстрів, що втілюють естетику
+            та комфорт у вашому будинку.
+          </p>
+          <p className={styles.aboutUsContent}>
+            {' '}
+            Наша місія - зробити ваше життя комфортним та елегантним, створюючи
+            індивідуальні рішення, які поєднують функціональність та
+            естетичність.
+          </p>
+          <p className={styles.aboutUsContent}>
+            Завдяки ретельному вибору матеріалів найвищої якості, ми
+            забеспечуємо довговічність та естетичність наших виробів.
+          </p>
+        </div>
       </div>
-      <img className={styles.floatingImg} src="" alt="" />
+      <Image
+        src="/images/aboutUs/floatingImg.png"
+        priority
+        alt="корпус меблі"
+        width={262}
+        height={219}
+        className={styles.floatingImg}
+      />
     </section>
   );
 }
