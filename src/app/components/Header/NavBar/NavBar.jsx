aec7d3f331770bbc2b906313004f004ce82dd99f
Ihor <ihorfront88@gmail.com>
2023-06-13 19:55:03 +0300

    fix bug with import

--- src/app/components/Header/NavBar/NavBar.jsx
+++ src/app/components/Header/NavBar/NavBar.jsx
@@ -1,14 +1,31 @@
+'use client';
+
 import Link from 'next/link';
 import items from './navBarData';
+import s from './NavBar.module.scss';
+import { useState } from 'react';
 
 const NavBar = () => {
+  const [show, setShow] = useState(false);
+  console.log('show:', show);
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+
   const elements = items.map(({ id, text, link }) => (
-    <li key={id}>
+    <li key={id} className={s.item}>
       <Link href="">{text}</Link>
     </li>
   ));
 
-  return <ul>{elements}</ul>;
+  return (
+    <>
+      <div onClick={handleClick} className={s.burger}></div>
+      {/* <ul className={s.list}>{elements}</ul> */}
+      <ul className={`${s.list} ${show ? s.active : ''}`}>{elements}</ul>
+    </>
+  );
 };
 
 export default NavBar;
