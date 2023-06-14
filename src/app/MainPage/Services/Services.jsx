3ef74d66705b706ce956585bb8309ace3c05e067
Your Name <wtm@hotmail.com.au>
2023-06-14 12:38:03 +0300

    footer/ services section added

---
+++ src/app/MainPage/Services/Services.jsx
@@ -0,0 +1,21 @@
+
+
+import styles from './Services.module.scss';
+export default function Services() {
+  return (
+    <section>
+      <div className={styles.services} style={{ backgroundImage: `url('/images/services/image1.jpg')`}}>
+        <p className={styles.servicesText} >Професійне обслуговування</p>
+      </div>
+      <div className={styles.services} style={{ backgroundImage: `url('/images/services/image2.jpg')` }}>
+        <p className={styles.servicesText}>Без прихованих комісій</p>
+      </div>
+      <div className={styles.services} style={{ backgroundImage: `url('/images/services/image3.jpg')` }}>
+        <p className={styles.servicesText}>Приваблива ціна</p>
+      </div>
+      <div className={styles.services} style={{ backgroundImage: `url('/images/services/image4.jpg')` }}>
+        <p className={styles.servicesText}>Індивідуальний підхід</p>
+      </div>
+    </section>
+  );
+}
