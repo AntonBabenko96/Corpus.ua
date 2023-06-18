2accae5350380e0eb5e88b560413ef71310a1bef
Alexuber <alexuberbuber@gmail.com>
2023-06-18 17:02:53 +0300

    merged main

--- src/app/MainPage/Portfolio/slider/Slider.jsx
+++ src/app/MainPage/Portfolio/slider/Slider.jsx
@@ -3,6 +3,7 @@
 // import Swiper core and required modules
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useMediaQuery } from 'react-responsive';
 import styles from './Slider.module.scss';
 // Import Swiper styles
 import 'swiper/css';
@@ -16,28 +17,65 @@ const images = [
 ];
 
 export default () => {
+  const isMobileMax = useMediaQuery({ maxWidth: 375 });
   return (
     <Swiper
       className={styles.swiper}
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={30}
-      slidesPerView={2}
+      // slidesPerView={isMobileMax ? 1 : 2}
       navigation
       loop
       onSwiper={swiper => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
+      breakpoints={{
+        // Коли екран є 375px або більше
+        375: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        // Коли екран є 1440px або більше
+        900: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+      }}
     >
       <SwiperSlide>
-        <img src={images[0]} alt="furniture" width="570" height="355" />
+        <img
+          src={images[0]}
+          alt="furniture"
+          width="570"
+          height="355"
+          className={styles.img}
+        />
       </SwiperSlide>
       <SwiperSlide>
-        <img src={images[1]} alt="furniture" width="570" height="355" />
+        <img
+          src={images[1]}
+          alt="furniture"
+          width="570"
+          height="355"
+          className={styles.img}
+        />
       </SwiperSlide>
       <SwiperSlide>
-        <img src={images[0]} alt="furniture" width="570" height="355" />
+        <img
+          src={images[0]}
+          alt="furniture"
+          width="570"
+          height="355"
+          className={styles.img}
+        />
       </SwiperSlide>
       <SwiperSlide>
-        <img src={images[1]} alt="furniture" width="570" height="355" />
+        <img
+          src={images[1]}
+          alt="furniture"
+          width="570"
+          height="355"
+          className={styles.img}
+        />
       </SwiperSlide>
     </Swiper>
   );
