df34bdbccd75126ad39d462562956146888097be
Alexuber <alexuberbuber@gmail.com>
2023-06-19 14:41:05 +0300

    added slider

--- src/app/MainPage/Portfolio/Portfolio.jsx
+++ src/app/MainPage/Portfolio/Portfolio.jsx
@@ -1,12 +1,19 @@
 import Slider from './slider/Slider';
 import styles from './Portfolio.module.scss';
 
+const images = [
+  { id: '1', src: '/images/portfolio/image31.jpg', desc: 'furniture' },
+  { id: '2', src: '/images/portfolio/image32.jpg', desc: 'furniture' },
+  { id: '3', src: '/images/portfolio/image31.jpg', desc: 'furniture' },
+  { id: '4', src: '/images/portfolio/image32.jpg', desc: 'furniture' },
+];
+
 const Portfolio = () => {
   return (
     <section className={styles.portfolio}>
       <div className={styles.blackAccent}></div>
       <h2 className={styles.title}>НАШІ ПРОЕКТИ</h2>
-      <Slider />
+      <Slider images={images} />
     </section>
   );
 };
--- src/app/MainPage/Portfolio/slider/Slider.jsx
+++ src/app/MainPage/Portfolio/slider/Slider.jsx
@@ -3,27 +3,18 @@
 // import Swiper core and required modules
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { useMediaQuery } from 'react-responsive';
 import styles from './Slider.module.scss';
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-const images = [
-  '/images/portfolio/image31.jpg',
-  '/images/portfolio/image32.jpg',
-];
-
-export default () => {
-  const isMobileMax = useMediaQuery({ maxWidth: 375 });
+export default ({ images }) => {
   return (
     <Swiper
       className={styles.swiper}
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={30}
-      // slidesPerView={isMobileMax ? 1 : 2}
       navigation
       loop
       onSwiper={swiper => console.log(swiper)}
@@ -35,48 +26,23 @@ export default () => {
           spaceBetween: 20,
         },
         // Коли екран є 1440px або більше
-        900: {
+        1440: {
           slidesPerView: 2,
           spaceBetween: 30,
         },
       }}
     >
-      <SwiperSlide>
-        <img
-          src={images[0]}
-          alt="furniture"
-          width="570"
-          height="355"
-          className={styles.img}
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src={images[1]}
-          alt="furniture"
-          width="570"
-          height="355"
-          className={styles.img}
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src={images[0]}
-          alt="furniture"
-          width="570"
-          height="355"
-          className={styles.img}
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          src={images[1]}
-          alt="furniture"
-          width="570"
-          height="355"
-          className={styles.img}
-        />
-      </SwiperSlide>
+      {images.map(({ id, src, desc }) => (
+        <SwiperSlide key={id}>
+          <img
+            src={src}
+            alt={desc}
+            width="570"
+            height="355"
+            className={styles.img}
+          />
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 };
