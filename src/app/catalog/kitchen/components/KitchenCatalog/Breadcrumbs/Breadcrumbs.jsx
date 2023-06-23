07d19df416f570216dcc3cbfc1103c71ac94521d
Ihor <ihorfront88@gmail.com>
2023-06-23 17:22:00 +0300

    add: dynamic page about Kitchen mobile version

--- src/app/catalog/kitchen/components/KitchenCatalog/Breadcrumbs/Breadcrumbs.jsx
+++ src/app/catalog/kitchen/components/KitchenCatalog/Breadcrumbs/Breadcrumbs.jsx
@@ -1,11 +1,12 @@
 import s from './Breadcrumbs.module.scss';
 
-const Breadcrumbs = () => {
+const Breadcrumbs = ({ id }) => {
   return (
     <ul className={s.breadcrumbsList}>
       <li className={s.item}>Головна</li>
       <li className={s.item}>Каталог</li>
       <li className={s.item}>Кухні</li>
+      {id && <li className={s.item}>{id}</li>}
     </ul>
   );
 };
