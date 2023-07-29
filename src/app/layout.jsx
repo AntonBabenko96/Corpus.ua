6b4f74f1b7804e90100d0e10d229bc0ece2cf400
AntonBabenko96 <ababenko159@gmail.com>
2023-07-29 12:38:12 +0300

    qq

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,8 @@
 // import 'styles/index.scss';
-import '../styles/index.scss';
-import './global.scss';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+// import '../styles/index.scss';
+// import './global.scss';
+// import Header from './components/Header/Header';
+// import Footer from './components/Footer/Footer';
 
 export const metadata = {
   title: 'KORPUS',
@@ -13,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header />
+        {/* <Header /> */}
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,26 @@
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
-import AboutUs from './MainPage/AboutUs/AboutUs';
-import Hero from './MainPage/Hero/Hero';
-import Services from './MainPage/Services/Services';
-import Promotions from './components/Promotions/Promotions';
-import ContactForm from './components/ContactForm/ContactForm';
-import Portfolio from './MainPage/Portfolio/Portfolio';
-import FooterImage from './components/FooterImage/FooterImage';
+// import AboutUs from './MainPage/AboutUs/AboutUs';
+// import Hero from './MainPage/Hero/Hero';
+// import Services from './MainPage/Services/Services';
+// import Promotions from './components/Promotions/Promotions';
+// import ContactForm from './components/ContactForm/ContactForm';
+// import Portfolio from './MainPage/Portfolio/Portfolio';
+// import FooterImage from './components/FooterImage/FooterImage';
 // import NotFaund from './NotFoundPage/NotFound';
 
 export default function Home() {
   return (
     <main>
-      <Hero />
+      {/* <Hero />
       <AboutUs />
       <Services />
       <Portfolio />
       <ContactForm />
       <Promotions />
       <FooterImage/>
-      <ToastContainer />
+      <ToastContainer /> */}
       {/* <NotFaund/> */}
     </main>
   );
