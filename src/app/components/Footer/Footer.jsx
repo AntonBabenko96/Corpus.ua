807f1598ce7afd7e629727b636991b04eedfea11
AntonBabenko96 <ababenko159@gmail.com>
2023-07-31 02:30:45 +0300

    add footer

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import Container from '../Container/Container';
 import styles from './Footer.module.scss';
+import logo from '../../../image/Logo.png';
 
 async function getCategory() {
   const response = await fetch(`https://korpus.onrender.com/api/categories`);
@@ -17,30 +19,51 @@ export default async function Footer() {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.logo}></div>
-      <div className={styles.aboutUs}>
-        <h3>Про нас</h3>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-      <div className={styles.catalog}></div>
-      <h3>Категорії</h3>
-      <ul>
-        {categoryList.map(el => (
-          <li>{el.name}</li>
-        ))}
-      </ul>
-      <div className={styles.contacts}>
-        <h3>Контакти</h3>
-        <ul>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
+      <Container>
+        <div className={styles.wrapper}>
+          <div className={styles.logo}>
+            <Image src={logo} />
+          </div>
+          <div className={styles.aboutUs}>
+            <h3 className={styles.title}>Про нас</h3>
+            <ul className={styles.list}>
+              <li>Про Компанію</li>
+              <li>Наші проекти</li>
+              <li>Акції</li>
+              <li>Відгуки</li>
+            </ul>
+          </div>
+          <div className={styles.catalog}>
+            <h3 className={styles.title}>Каталог</h3>
+            <ul className={styles.list}>
+              {categoryList.map(el => (
+                <li>
+                  <Link href={`/${el.name}`}>{el.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className={styles.contacts}>
+            <h3 className={styles.title}>Контакти</h3>
+            <ul className={styles.list}>
+              <li>
+                <a href="tel:+38 095 555 55 22">+38 095 555 55 22</a>
+              </li>
+              <li>
+                <a href="mailto:korpus@gmail.com">korpus@gmail.com</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.root}>
+          <p className={styles.rootText}>
+            2023 © Всі права захищені. Копіювання заборонено.
+          </p>
+          <br />
+          <p className={styles.rootText}>Сайт розроблено компанією “DevCore”</p>
+        </div>
+      </Container>
     </footer>
   );
 }
