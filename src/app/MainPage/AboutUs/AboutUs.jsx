6ae0c07d3d42d71e09c699ea6f6132f1b99ea968
Zhvanko Anna <zhvankoann@gmail.com>
2023-08-08 23:18:33 +0300

    Added section About Us for mobile

--- src/app/MainPage/AboutUs/AboutUs.jsx
+++ src/app/MainPage/AboutUs/AboutUs.jsx
@@ -1,51 +1,43 @@
 import Image from 'next/image';
-import styles from './AboutUs.module.scss';
+import Container from '@/app/components/Container/Container';
+import s from './AboutUs.module.scss';
 
 export default function AboutUs() {
   return (
-    // <div className="container">
-    <section id="about" className={styles.aboutUs}>
-      <Image
-        src="/images/aboutUs/aboutUs.png"
-        priority
-        alt="корпус меблі"
-        width={358}
-        height={348}
-         className={styles.aboutUsImg}
-      />
-      <div className={styles.aboutUsBackground}>
-        <div className={styles.aboutUsFrame}></div>
-
-        <h2 className={styles.aboutUsTitle}>KORPUS</h2>
-        <div className={styles.aboutUsText}>
-          <p className={styles.aboutUsContent}>
+    <section className={s.aboutUs}>
+      <div className={s.wrapper}>
+        <Container className={s.aboutUsContainer}>
+          <h2 className={s.title}>korpus</h2>
+          <p className={s.text}>
             Ми команда талановитих дизайнерів та майстрів, що втілюють естетику
             та комфорт у вашому будинку.
           </p>
-          <p className={styles.aboutUsContent}>
-            {' '}
+          <p className={s.text}>
             Наша місія - зробити ваше життя комфортним та елегантним, створюючи
             індивідуальні рішення, які поєднують функціональність та
             естетичність.
           </p>
-          <p className={styles.aboutUsContent}>
+          <p className={s.text}>
             Завдяки ретельному вибору матеріалів найвищої якості, ми
-            забеспечуємо довговічність та естетичність наших виробів.
+            забеспечуємо довговічність та естетичність.
           </p>
-        </div>
-        <div className={styles.aboutNightStandImg}>
-          <Image
-            src="/images/aboutUs/floatingImg.png"
-            priority
-            alt="корпус меблі"
-            width={262}
-            height={219}
-            className={styles.floatingImg}
-          />
-        </div>
+        </Container>
+        <div className={s.border}></div>
       </div>
-    
+      <Image
+        src="/images/aboutUs/aboutUs1@2x.png"
+        width={370}
+        height={360}
+        alt="Зображення інтер'єру"
+        className={s.leftImage}
+      />
+      <Image
+        src="/images/aboutUs/aboutUs2@2x.png"
+        width={262}
+        height={219}
+        alt="Зображення тумби"
+        className={s.rightImage}
+      />
     </section>
-    
   );
 }
